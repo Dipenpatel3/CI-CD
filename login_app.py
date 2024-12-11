8890acc67c52e593a42d1e1df9130b4d8aabeefc
Dipenpatel3 <patel.dipe@northeastern.edu>
2024-12-11 18:37:02 -0500

    new

--- login_app.py
+++ login_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 def login():
     """Handles the user login process."""
     st.session_state.logged_in = st.session_state.get('logged_in', False)
+    
     if not st.session_state.logged_in:
         username = st.text_input('Enter the Username:')
         password = st.text_input('Enter the password:', type='password')
@@ -20,10 +21,6 @@ def login():
             st.session_state.logged_in = False
             st.experimental_rerun()
 
-def main():
-    """Main application function."""
-    st.title('User Login')
-    login()
+st.title('User Login')
+login()
 
-if __name__ == '__main__':
-    main()
